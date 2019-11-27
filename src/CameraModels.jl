fd1b6856af3e0e651b4c77b7d1dc03ea8c80fa0c
yakir12 <12.yakir@gmail.com>
2019-11-27 07:57:24 +0100

    add basic types and a real2camera function

--- src/CameraModels.jl
+++ src/CameraModels.jl
@@ -1,3 +1,33 @@
 module CameraModels
 
+using StaticArrays, CoordinateTransformations
+
+export Intrinsic, Extrinsic, Pinhole, CameraModel, real2pixel
+
+abstract type CameraModel end
+
+struct Intrinsic
+    prinicipalpoint::Tuple{Float64, Float64} # in pixels
+    focallength::Tuple{Float64, Float64} # in pixels
+    skewcoefficient::Float64
+end
+
+struct Extrinsic
+    location::SVector{3, Float64}
+    direction::SVector{3, Float64}
+end
+
+struct Pinhole <: CameraModel
+    intrinsic::Intrinsic
+    extrinsic::Extrinsic
+end
+
+"""
+    real2pixel(m::PinHole)
+Return a transformation that converts real-world coordinates
+to camera coordinates. This currently ignores any tangential 
+distortion between the lens and the image plane.
+"""
+real2pixel(m::Pinhole) = cameramap(m.intrinsic.focallength, m.intrinsic.prinicipalpoint) ∘ inv(AffineMap(RotXYZ(m.extrinsic.direction...), m.extrinsic.location))
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,10 @@
 using CameraModels
 using Test
+
+@testset "real to camera" begin
+    intrinsic = Intrinsic((0., 0.), (1., 0), 0.0)
+    extrinsic = Extrinsic([0., 0, 0], [1., 0, 0])
+    m = Pinhole(intrinsic, extrinsic)
+    f = real2pixel(m)
+    @test f([0,0,1]) == [0, 0]
+end
