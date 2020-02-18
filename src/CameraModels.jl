69dc77759b4f25764f9945cd871f2a7a5a451edc
Evert Schippers <schippers.evert@gmail.com>
2020-02-19 00:03:28 +1000

    Added more interface and basic Pinhole example

--- src/CameraModels.jl
+++ src/CameraModels.jl
@@ -2,18 +2,70 @@ module CameraModels
 
 using StaticArrays, CoordinateTransformations
 
-export Ray, CameraModel, pixel2ray, point2pixel, Pinhole
+export Ray, CameraModel, PixelCoordinate, Vector3, Vector2, Point3
+export pixel2ray, point2pixel, canreproject, origin, direction, sensorsize, columns, rows, lookdirection, updirection
 
 const PixelCoordinate = SVector{2, Float64}
+const Vector2 = SVector{2, Float64}
+
 const Point3 = SVector{3, Float64}
-const Vector3 = Point3
+const Vector3 = SVector{3, Float64}
 
 abstract type CameraModel end
+
+origin3d = zeros(Point3)
 
 struct Ray
     origin::Point3
     direction::Vector3
 end
+
+"""
+    origin(ray)
+
+Return the direction of the ray as a (Vector3)
+"""
+function origin end
+
+origin(vector::Vector3) = origin3d
+origin(ray::Ray) = vector.origin
+
+
+"""
+    direction(ray)
+
+Return the origin of ray, typically just a zero $(Point3) for normal cameras.
+"""
+function direction end
+
+direction(vector::Vector3) = vector
+direction(ray::Ray) = vector.direction
+
+
+"""
+    columns(model::CameraModel)
+
+Returns the width of the camera sensor.
+"""
+function columns end
+
+"""
+    rows(model::CameraModel)
+
+Returns the height of the camera sensor.
+"""
+function rows end
+
+"""
+    sensorsize(model::CameraModel)
+
+Return the size of the camera sensor. By default calling out to columns(model) and rows(model) to build an SVector{2}
+
+`sensorsize(cameramodel::CameraModel) = SVector{2}(columns(cameramodel), rows(cameramodel))`
+"""
+function sensorsize end
+
+sensorsize(cameramodel::CameraModel) = SVector{2}(columns(cameramodel), rows(cameramodel))
 
 """
     pixel2ray(cameramodel::CameraModel, pixelcoordinate::$(PixelCoordinate))
@@ -29,32 +81,27 @@ Returns the pixel location onto which the 3D coordinate `pointincamera` is proje
 """
 function point2pixel end
 
+"""
+    lookdirection(camera::CameraModel)
 
-
-struct Pinhole <: CameraModel
-    prinicipalpoint::Tuple{Float64, Float64} # in pixels
-    focallength::Tuple{Float64, Float64} # in pixels
-    skewcoefficient::Float64
-end
+Return the lookdirection of this camera model.
+"""
+function lookdirection end
 
 """
-    point2pixel(m::PinHole)
-Return a transformation that converts real-world coordinates
-to camera coordinates. This currently ignores any tangential 
-distortion between the lens and the image plane.
+    updirection(camera::CameraModel)
+
+Return the updirection of this camera model.
 """
-point2pixel(m::Pinhole) = cameramap(m.intrinsic.focallength, m.intrinsic.prinicipalpoint) ∘ inv(AffineMap(RotXYZ(m.extrinsic.direction...), m.extrinsic.location))
+function updirection end
 
-# TODO: Also implement pixel2ray for PinHole
+"""
+    canreproject(camera::CameraModel)
 
-# MORE IDEAS:
-# - Something like CoordinateTransformations' transform_deriv / transform_deriv_params, will be useful for estimators
-# - Could a camera model BE a CoordinateTransformations.Transform ?
-# - In doubt if pixel2ray/point2pixel should return a transform or DO the transform... The former will lead to ugly code, I find...
-# - I don't like the Ray struct... For many users, all rays will have zero origin anyways. I'm in accurate underwater and 360 business,
-#   where the shift of the origin matters. I'll be extending this later with some very exotic camera models, which will need something
-#   like this. Needs some more thought... 
-#   --> Maybe a pixel2rayorigin, which defaults to calling to the "simple" pixel2ray and adding a default zero origin. When you do the
-#       accurate stuff, you would call pixel2rayorigin rather then pixel2ray. And only the funky models would have to implement it.    
+Confirms if point2pixel is implemented for this camera model.
+"""
+canreproject(camera::CameraModel) = true
+
+include("Pinhole.jl")
 
 end # module
